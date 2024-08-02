6f2066b09479c61f71124cdd4d08480b53cb6360
ydideh810 <yd.baller810@gmail.com>
2024-08-02 16:40:04 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Zen+Dots&display=swap">
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
