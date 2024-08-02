0cb2e240e8e153beb0b5138a374239d5addffe22
ydideh810 <yd.baller810@gmail.com>
2024-08-02 00:37:00 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
-
-      <body className="bg-gray-200 bg-llamax">
+      <Head>
+        <style>
+          {`
+            body {
+              background-image: url('/path/to/your/image.jpg'); /* Replace with your image path */
+              background-size: cover;
+              background-position: center;
+              background-repeat: no-repeat;
+            }
+          `}
+        </style>
+      </Head>
+      <body className="bg-gray-200">
         <Main />
         <NextScript />
       </body>
