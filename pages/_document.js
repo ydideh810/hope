f16f3ecdaf272695f75edd25c6d319e436dc4199
ydideh810 <yd.baller810@gmail.com>
2024-08-02 11:40:55 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html className="h-full">
       <Head>
         <style>
           {`
@@ -10,11 +10,16 @@ export default function Document() {
               background-image: url('bigspace.png'); 
               background-size: cover;
               background-position: center;
+              background-repeat: no-repeat;
+            }
+        body {
+              height: 100%;
+              margin: 0;
             }
           `}
         </style>
       </Head>
-      <body className="bg-llama bg-cover bg-center">
+      <body className="bg-llama bg-cover bg-center bg-no-repeat h-full">
         <Main />
         <NextScript />
       </body>
